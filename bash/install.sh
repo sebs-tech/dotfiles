1370d18a4f3e2c78da185be8752594c7f8db4ff0
Seb's tech <sebastjan.rijavec@gmail.com>
2024-11-30 18:16:34 +0100

    Update install.sh

--- bash/install.sh
+++ bash/install.sh
@@ -2,6 +2,7 @@
 
 # install required packages 
 # sudo xargs -a ./packages.txt snap install -y
+sudo snap install tree
 
 # copy aliases files into the home directory 
 cp bash_aliases ~/.bash_aliases 
