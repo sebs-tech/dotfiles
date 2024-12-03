2f260941320a468da714e4e4c0f119ef55352dc9
Seb's tech <sebastjan.rijavec@gmail.com>
2024-12-03 13:26:35 +0100

    Create create_them_wsl.bat

---
+++ wsl/create_them_wsl.bat
@@ -0,0 +1,25 @@
+########################################
+# Creates my squadron of wsl machines. #
+########################################
+
+@echo off 
+echo Creating Mildred 
+wsl --import mildred D:\WSL\ladies\mildred D:\WSL\backup\Ubuntu-24.04.tar
+
+echo Creating Beatrice
+wsl --import beatrice D:\WSL\ladies\beatrice D:\WSL\backup\Ubuntu-24.04.tar
+
+echo Creating Charlotte
+wsl --import charlotte D:\WSL\ladies\charlotte D:\WSL\backup\Ubuntu-24.04.tar
+
+echo Creating Constance
+wsl --import constance D:\WSL\ladies\constance D:\WSL\backup\Ubuntu-24.04.tar
+
+echo Creating Emmeline
+wsl --import emmeline D:\WSL\ladies\emmeline D:\WSL\backup\Ubuntu-24.04.tar
+
+echo Creating Gwendaline
+wsl --import gwendaline D:\WSL\ladies\gwendaline D:\WSL\backup\Ubuntu-24.04.tar
+
+echo Creating Lilian
+wsl --import lilian D:\WSL\ladies\lilian D:\WSL\backup\Ubuntu-24.04.tar
